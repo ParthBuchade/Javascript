aa5a3a2b758d7a2f746fe8f2ea5929cdf10607ce
ParthBuchade <parthbuchade1508@gmail.com>
2024-12-25 15:43:35 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,24 @@
+"use strict"; // further code will treated as a newer version of javascript
+
+//alert("hello") // we are using node js not browser the pop-up will not show here
+
+let name = "Parth"
+let age = 21
+let isLoggedIn = false
+
+
+//number => range is 2 to power 53
+// string => "" 
+// boolean => true/false
+// null => standAlone value
+// undifined
+// symbols used for uniqueness
+
+//using typeof
+console.log(typeof name);
+console.table([typeof age,typeof isLoggedIn])
+console.log(typeof undefined); // type is undefined
+console.log(typeof null); // type is object
+
+
+
